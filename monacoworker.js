16c5acb670b818f8b2486ecad6e5ad6d776428c9
Sam El-Husseini <samelh@microsoft.com>
2018-04-12 13:43:35 -0700

    Initial.

--- monacoworker.js
+++
@@ -1,5 +0,0 @@
-// see https://github.com/Microsoft/monaco-editor
-self.MonacoEnvironment = { 
-    baseUrl: "/test-pxt-sample/" 
-};
-importScripts("/test-pxt-sample/vs/base/worker/workerMain.js");
